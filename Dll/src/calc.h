48e379f8af3838c0f2554d55a9dbbbdadb2af776
1m188 <228117330@qq.com>
2019-02-16 21:41:40 +0800

    给出了大概的头文件框架，有对于测试和正式生成发布版本的宏定义，有C风格的对外调用接口，有其他的用于计算中间步骤的函数。

---
+++ Dll/src/calc.h
@@ -0,0 +1,37 @@
+#ifndef CALC_H
+#define CALC_H
+
+//定义一些宏，DEBUG的时候进行测试调试，RELEASE的时候生成发布版本dll
+#ifdef DEBUG
+#define DLL_EXPORT
+#elif defined RELEASE
+#define DLL_EXPORT __declspec(dllexport)
+#endif
+
+//用C的函数方式导出
+#ifdef RELEASE
+extern "C"
+{
+#endif
+
+    //对外调用的计算接口
+    DLL_EXPORT int calc(char *s, int length);
+
+#ifdef RELEASE
+}
+#endif
+
+//以下是计算接口中调用的一些内部计算函数
+#include "vector"
+#include "string"
+
+//从字符串转换为中缀表达式
+std::vector<std::string> str2In(char *s, int length);
+
+//从中缀表达式转换为后缀表达式
+std::vector<std::string> in2Post(const std::vector<std::string> &inFixExpression);
+
+//通过后缀表达式计算
+int calc(const std::vector<std::string> &postFixExpression);
+
+#endif
