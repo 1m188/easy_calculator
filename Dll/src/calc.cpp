14920a1430c87e39f8ae59f707b0a79a3742b07e
1m188 <228117330@qq.com>
2019-02-16 21:43:50 +0800

    仔细想了一下，考虑到自己对于clang的C部分还是不清不楚的，所以还是使用了C++的那一套以及和C++兼容的C语言部分，声明都写上去了就差实现了，另外还有一个测试的部分。

---
+++ Dll/src/calc.cpp
@@ -0,0 +1,31 @@
+#include "calc.h"
+
+DLL_EXPORT int calc(char *s, int length)
+{
+    //中缀表达式
+    return 0;
+}
+
+std::vector<std::string> str2In(char *s, int length)
+{
+}
+
+std::vector<std::string> in2Post(const std::vector<std::string> &inFixExpression)
+{
+}
+
+int calc(const std::vector<std::string> &postFixExpression)
+{
+}
+
+//测试
+#ifdef DEBUG
+
+#include "iostream"
+
+int main()
+{
+    return 0;
+}
+
+#endif
