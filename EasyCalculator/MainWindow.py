6e349b70892212995c8b25a299fc8bcba1de9ab5
1m188 <228117330@qq.com>
2019-02-17 14:54:52 +0800

    窗口移动到屏幕中央

--- EasyCalculator/MainWindow.py
+++ EasyCalculator/MainWindow.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QWidget
+from PySide2.QtWidgets import QWidget, QApplication
 
 
 #主窗口
@@ -9,3 +9,8 @@ class MainWindow(QWidget):
         #设置窗口大小和标题
         self.setWindowTitle("简单四则运算器")
         self.resize(600, 600)
+
+        #移动到屏幕中央
+        rect = self.frameGeometry()
+        rect.moveCenter(QApplication.desktop().availableGeometry().center())
+        self.move(rect.topLeft())
