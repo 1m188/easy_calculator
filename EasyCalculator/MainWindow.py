cf74b3c88c501e4ffacafa10aeb8a2c640e56593
1m188 <228117330@qq.com>
2019-02-18 16:42:52 +0800

    加入了窗口部分属性的设定，另外修改了窗口右上角的按钮风格，最后使窗口的大小固定。

--- EasyCalculator/MainWindow.py
+++ EasyCalculator/MainWindow.py
@@ -1,4 +1,5 @@
 from PySide2.QtWidgets import QWidget, QApplication
+from PySide2.QtCore import Qt
 
 
 #主窗口
@@ -6,9 +7,16 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__(None)
 
+        #设置窗口属性
+        self.setAttribute(Qt.WA_DeleteOnClose, True)
+        self.setAttribute(Qt.WA_QuitOnClose, True)
+
+        #设置窗口样式
+        self.setWindowFlags(Qt.WindowCloseButtonHint)
+
         #设置窗口大小和标题
         self.setWindowTitle("简单四则运算器")
-        self.resize(600, 600)
+        self.setFixedSize(500, 600)
 
         #移动到屏幕中央
         rect = self.frameGeometry()
