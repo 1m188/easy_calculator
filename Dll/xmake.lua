1ac1c4b71646178a09a3766af0b2bd76c277cd42
1m188 <228117330@qq.com>
2019-10-19 22:00:52 +0800

    添加自动复制release构建动态链接库文件到py动态链接库文件夹下的功能

--- Dll/xmake.lua
+++ Dll/xmake.lua
@@ -3,6 +3,13 @@ add_rules("mode.debug","mode.release")
 target("calc")
     set_kind("shared")
     add_files("src/calc/*.cpp")
+    
+    -- 判定如果是release模式构建的话则把对应的动态链接库文件复制到py的动态链接库加载文件夹下，方便直接使用无需另外复制
+    if is_mode("release") then
+        after_build(function (target)
+            os.cp(target:targetfile(),"../EasyCalculator/dll")
+        end)
+    end
 
 target("test")
     set_kind("binary")
